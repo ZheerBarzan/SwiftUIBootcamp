d5241de920463c344361bcf0ce4c8964a166ebfa
zheer barzan <zheerbarzan85@outlook.com>
2024-12-02 17:53:06 +0300

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Zheer Barzan on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
