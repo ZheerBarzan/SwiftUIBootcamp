484abe9866db64e0f8941191c50d3c16c121b496
zheer barzan <zheerbarzan85@outlook.com>
2024-12-03 16:11:05 +0300

    adding shapes to SwiftUI

---
+++ SwiftUIBootcamp/components/shapes.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct ShapesComponent: View {
+    var body: some View {
+        ZStack {
+            // circle built in swift
+            Circle().fill(Color.red).stroke(Color.black, lineWidth: 5)
+            // rectangle built in swift
+            Rectangle().fill(Color.green).stroke(Color.black, lineWidth: 5).frame(width: 250, height: 250)
+            // tringle made a whole new file called triangle
+            Triangle().fill(Color.blue).stroke(Color.black, lineWidth: 5).frame(width: 250, height: 250)
+        }
+    }
+}
+
+#Preview {
+    ShapesComponent()
+}
