9f5d54ca3e05693d18674023c282ce4c0839daec
zheer barzan <zheerbarzan85@outlook.com>
2024-12-05 19:11:06 +0300

    a little challangae for my self

--- SwiftUIBootcamp/components/background.swift
+++ SwiftUIBootcamp/components/background.swift
@@ -4,7 +4,9 @@ import SwiftUI
 
 struct BackgroundComponent: View {
     var body: some View {
-       Text("Hello, World!")
+
+        VStack{
+            Text("Hello, World!")
        .background(
     
         ShapesComponent()
@@ -18,8 +20,37 @@ struct BackgroundComponent: View {
        )
        .ignoresSafeArea()
                 
+
         }
+        
+       
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .background(
+                    
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.black, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)).frame(width: 100, height: 100)
+                        .shadow(color: Color.primary.opacity(0.5),radius: 10, x: 0, y: 10)
+                        .overlay(
+                            Circle()
+                                .fill(Color.red)
+                                .frame(width: 35, height: 35)
+                                .overlay(
+                                    Text("5")
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                )
+                            
+                            , alignment: .bottomTrailing
+                        )
+                )
+                
+                .ignoresSafeArea()
+        
     }
+       
+}
 
 #Preview {
     BackgroundComponent()
