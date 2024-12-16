22f5ee58c8404382d0f88481991142f09fc2f422
zheer barzan <zheerbarzan85@outlook.com>
2024-12-16 18:50:19 +0300

    To do

---
+++ SwiftUIBootcamp/components/ternaryandconditionals.swift
@@ -0,0 +1,18 @@
+//
+//  ternaryandconditionals.swift
+//  SwiftUIBootcamp
+//
+//  Created by Zheer Barzan on 16/12/24.
+//
+
+import SwiftUI
+
+struct ternaryandconditionals: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ternaryandconditionals()
+}
