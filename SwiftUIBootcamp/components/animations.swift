5fd23446f289615c5f2bc045984e3f6cd1b0e15c
zheer barzan <zheerbarzan85@outlook.com>
2024-12-16 18:51:11 +0300

    animation

---
+++ SwiftUIBootcamp/components/animations.swift
@@ -0,0 +1,18 @@
+//
+//  animations.swift
+//  SwiftUIBootcamp
+//
+//  Created by Zheer Barzan on 16/12/24.
+//
+
+import SwiftUI
+
+struct animations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    animations()
+}
