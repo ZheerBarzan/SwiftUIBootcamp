664b598bb77d6eb5970e3a370579dcf5504a3049
zheer barzan <zheerbarzan85@outlook.com>
2024-12-17 19:26:45 +0300

    animation done

--- SwiftUIBootcamp/components/animations.swift
+++ SwiftUIBootcamp/components/animations.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct animations: View {
+    
+    @State var isAnimated = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Button(action:{
+                withAnimation(.easeInOut(duration: 1)){
+                    isAnimated.toggle()
+                }
+            }, label: {
+                Text("Animate")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
+            if isAnimated {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.scale)
+                    .overlay(Text("Hello, World!").foregroundColor(.white))
+            }
+            
+            
+        }
     }
 }
 
