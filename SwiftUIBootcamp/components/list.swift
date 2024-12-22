72fb9729d33bb4e838f7aafac89ee0ae001dbe4c
zheer barzan <zheerbarzan85@outlook.com>
2024-12-22 22:31:02 +0300

    list, add , move , delete, create covered

---
+++ SwiftUIBootcamp/components/list.swift
@@ -0,0 +1,82 @@
+//
+//  ListView.swift
+//  SwiftUIBootcamp
+//
+//  Created by Zheer Barzan on 22/12/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    @State private var f1Teams = [
+        "Mercedes",
+        "Ferrari",
+        "Red Bull",
+        "McLaren",
+        "Aston Martin",
+        "Alpine",
+        "Alpha Tauri",
+        "Alfa Romeo",
+        "Williams",
+        "Haas"
+    ]
+    
+    @State private var Drivers: [String:String] = [
+        "Mercedes": "Lewis Hamilton",
+        "Ferrari": "Charles Leclerc",
+        "Red Bull": "Max Verstappen",
+        "McLaren": "Lando Norris",
+        "Aston Martin": "Sebastian Vettel",
+        "Alpine": "Fernando Alonso",
+        "Alpha Tauri": "Pierre Gasly",
+        "Alfa Romeo": "Kimi Raikkonen",
+        "Williams": "George Russell",
+        "Haas": "Nico Hulkenberg"
+        
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("F1 Teams")) {
+                    ForEach(f1Teams, id: \.self) { team in
+                        Text(team.capitalized)
+                    }
+                    .onDelete(perform: deleteItems)
+                    .onMove(perform: moveItems)
+                }
+                
+                Section(header: Text("Drivers")) {
+                    ForEach(f1Teams, id: \.self) { team in
+                        Text("\(Drivers[team]!), \(team)")
+                    }
+                }
+            }
+            .accentColor(.purple)
+            .listStyle(DefaultListStyle())
+            .navigationTitle("Formula 1 Teams")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: addItem, label: {
+                Image(systemName: "plus")
+            }))
+        }
+        .accentColor(.red)
+    }
+    
+    // Deletes selected items from the list
+    func deleteItems(at offsets: IndexSet) {
+        f1Teams.remove(atOffsets: offsets)
+    }
+    
+    // Moves items within the list
+    func moveItems(from source: IndexSet, to destination: Int) {
+        f1Teams.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func addItem() {
+        f1Teams.append("New Team")
+    }
+}
+
+#Preview {
+    ListView()
+}
