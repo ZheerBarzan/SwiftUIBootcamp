2a75408e00265a9f4b646e23d710dd78399bd8ab
zheer barzan <zheerbarzan85@outlook.com>
2024-12-26 18:38:25 +0300

    stepper coverd somehow usefull

---
+++ SwiftUIBootcamp/components/stepper.swift
@@ -0,0 +1,18 @@
+//
+//  stepper.swift
+//  SwiftUIBootcamp
+//
+//  Created by Zheer Barzan on 26/12/24.
+//
+
+import SwiftUI
+
+struct stepper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    stepper()
+}
