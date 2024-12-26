7664c417fafaa7777f5edac6d530a098692587c9
zheer barzan <zheerbarzan85@outlook.com>
2024-12-26 18:38:43 +0300

    ma bad

--- SwiftUIBootcamp/components/stepper.swift
+++ SwiftUIBootcamp/components/stepper.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct stepper: View {
+    @State private var age = 18
+    @State private var widthIncrement: CGFloat = 0
+    @State private var heightIncrement: CGFloat = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            VStack{
+                
+                Stepper("Age: \(age)", value: $age, in: 18...100)
+                    .padding()
+                Spacer()
+                
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(Color.red)
+                    .frame(width: 200 + widthIncrement, height: 100 + heightIncrement)
+                    
+                
+                Spacer()
+                
+                Stepper("Width: \(Int(widthIncrement))"){
+                    incrementWidth(for: 10)
+                } onDecrement: {
+                    incrementWidth(for: -10)
+                }
+                .padding()
+                
+                Stepper("Height: \(Int(heightIncrement))"){
+                    incrementHeight(for: 10)
+                } onDecrement: {
+                    incrementHeight(for: -10)
+                }.padding()
+                
+                Spacer()
+               
+                
+            }
+    }
+    
+    func incrementWidth(for amount: CGFloat){
+        withAnimation(.easeInOut(duration: 1)){
+            widthIncrement += amount
+        }
+    }
+    
+    func incrementHeight(for amount: CGFloat){
+        withAnimation(.easeInOut(duration: 1)){
+            heightIncrement += amount
+        }
     }
 }
 
