7462ce103887bb5ea8a8375a2929ef2d6ae20cff
zheer barzan <zheerbarzan85@outlook.com>
2025-01-13 19:01:06 +0300

    text selection covered

---
+++ SwiftUIBootcamp/components/textSelection.swift
@@ -0,0 +1,19 @@
+//
+//  textSelection.swift
+//  SwiftUIBootcamp
+//
+//  Created by Zheer Barzan on 13/1/25.
+//
+
+import SwiftUI
+
+struct textSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    textSelection()
+}
