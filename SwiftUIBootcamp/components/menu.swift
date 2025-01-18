74db9670cb4237766d5b8c8c6c1a64f0e883226d
ZheerBarzan <zheerbarzan85@outlook.com>
2025-01-18 18:47:22 +0300

    menu in swift

---
+++ SwiftUIBootcamp/components/menu.swift
@@ -0,0 +1,26 @@
+//
+//  menu.swift
+//  SwiftUIBootcamp
+//
+//  Created by zheer barzan on 18/1/25.
+//
+
+import SwiftUI
+
+struct menu: View {
+    var body: some View {
+        Menu("Menu") {
+            Button("Item 1") {
+            }
+            Button("Item 2") {
+            }
+            Button("Item 3") {
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    menu()
+}
